a8db90fcf9db9558560eed6e0d2ee5a2f1e0ee2b
47gfx <117771075+4700gfx@users.noreply.github.com>
2023-11-24 01:45:00 -0500

    Setting Element Values from DOM Element

---
+++ script.js
@@ -0,0 +1,6 @@
+//Elements Section 
+
+document.querySelector(".message").textContent = 'Check Again! 🙈';
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+document.querySelector(".guess").value = 2;
